d21aa9ffaf3e0a89b28a3416c35e05bd02695a5e
Sherrybmd <shahryar.babamohamadi.2006@gmail.com>
2025-05-24 21:03:32 +0330

    Fixed my fucking version

--- libraries/books.cpp
+++ libraries/books.cpp
@@ -3,43 +3,8 @@
 using namespace std;
 
 
-<<<<<<< HEAD
-class person{
-protected:
-   string name;
-   int id;
-public:
-   person(string name, int id);
-   string const Get_Name();
-   int const get_Id();
-   void set_Name(string newname);
-   void set_wId(int newid);
-};
-
-
-class author : public person{
-private:
-    int numberofbooks;
-public:
-    author();
-    author(string name,int id,int numberofbooks);
-    int const getnumberofbooks();
-    void setnumberofbooks(int count);
-};
-
-
-class customer : public person{
-public:
-    customer(string name, int Id);
-};
-
-
-class books{
-private:
-=======
 class Person {
 protected:
->>>>>>> a7d4aa6064a228ac272c2281c1b8bfd8c6d56cf7
     string name;
     int id;
 
@@ -81,33 +46,6 @@ private:
     int authorId;
     int id;
     double price;
-<<<<<<< HEAD
-public:
-    books(string title, int bookid,int authorid, double price);
-    string const get_title();
-    int const get_book_id();
-    int const get_author_id();
-    double const get_price();
-    void set_title(string title);
-    void set_book_id(int id);
-    void set_author_id(int aid);
-    void set_price(double price);
-};
-
-
-class sale : public books{
-public:
-    sale();
-    sale( int bookid,int quantity,int cutomerid,double price);
-    int const get_book_id();
-    int const get_quantity();
-    int const get_customerid();
-    double const get_price();
-    void set_book_id(int id);
-    void set_quantity(int quantity);
-    void set_customerid(int id);
-    void set_price(double price);
-=======
 
 public:
     Book();
@@ -145,5 +83,5 @@ public:
     void setQuantity(int quantity);
     void setCustomerId(int id);
     void setPrice(double price);
->>>>>>> a7d4aa6064a228ac272c2281c1b8bfd8c6d56cf7
 };
+
