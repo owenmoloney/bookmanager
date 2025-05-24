df04d6fe50d58b5a1cd3096e3b3ebb85c0e76b4a
Sherrybmd <shahryar.babamohamadi.2006@gmail.com>
2025-05-24 12:19:05 +0330

    Add cracked out the wazoo menu
    can use cin >> enumVariable;
    with the used operator overloading method.
    
    now that menu works focus should be on classes.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,40 @@
+#include <cstdlib>
 #include <iostream>
 using namespace std;
 
-int askNicely(){
-    unsigned int choice{0};
+enum options {
+    error = -1,
+    quit = 0,
+    addBook = 1,
+    removeBook,
+    searchBook,
+    searchAuthor,
+    listAllBooks,
+    listAllAuthors,
+};
+
+istream& operator>>(istream& is, options& option)
+{
+    int usr;
+    is>>usr;
+    option = static_cast<options>(usr);
+    return is;
+}
+options askNicely(){
+
+    options choice;
     cout << "choose action" << "\n";
-    cout << "1 > add new book" << "\n"
+    cout << "0 > quit" << "\n"
+        << "1 > add new book" << "\n"
         << "2 > remove existing book" << "\n"
         << "3 > search for book" << "\n"
         << "4 > search author" << "\n"
         << "5 > display all books" << "\n"
         << "6 > display all authors" << "\n";
     cin >> choice;
-    if (choice <=  0 || choice > 6)
-        return -1;
+
+    if (choice <  0 || choice > 6)
+        return error;
 
     return choice;
 }
@@ -22,31 +44,42 @@ int main()
     bool running = true;
     while (running)
     {
-        int option = askNicely();
+        options option = askNicely();
         switch(option)
         {
             //"1 > add new book"
-            case 1:
+            case addBook:
+                cout << "addBook" << '\n';
                 break;
 
             //"2 > remove existing book"
-            case 2:
+            case removeBook:
+                cout << "removeBook" << '\n';
                 break;
 
             // "3 > search for book"
-            case 3:
+            case searchBook:
+                cout << "findBook" << '\n';
                 break;
 
            // "4 > search author"
-            case 4:
+            case searchAuthor:
+                cout << "findAuth" << '\n';
                 break;
 
             // "5 > display all books"
-            case 5:
+            case listAllBooks:
+                cout << "listBooks" << '\n';
                 break;
 
             // "6 > display all authors"
-            case 6:
+            case listAllAuthors:
+                cout << "listAuth" << '\n';
+                break;
+
+            case quit:
+                cout << "quit" << '\n';
+                running = false;
                 break;
 
             // err handle, or is it
